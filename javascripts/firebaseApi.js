377bef6ddb93e85aa861a957d77a4d120408156f
Gina Antonini <ginamantonini@gmail.com>
2017-10-24 17:06:14 -0500

    get movies from firebase

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -3,23 +3,50 @@
 let firebaseKey = "";
 let userUid = "";
 
-const setKey = (key) => {
+const setKey = (key) =>{
 	firebaseKey = key;
 };
 
 //Firebase: GOOGLE - Use input credentials to authenticate user.
-  let authenticateGoogle = () => {
-    return new Promise((resolve, reject) => {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider)
-        .then((authData) => {
-        	userUid = authData.user.uid;
-            resolve(authData.user);
-        }).catch((error) => {
-            reject(error);
-        });
-    });
-  };
+let authenticateGoogle = () => {
+	return new Promise((resolve, reject) => {
+	  var provider = new firebase.auth.GoogleAuthProvider();
+	  firebase.auth().signInWithPopup(provider)
+	    .then((authData) => {
+	    	userUid = authData.user.uid;
+	        resolve(authData.user);
+	    }).catch((error) => {
+	        reject(error);
+	    });
+	});
+};
+
+const getMovieList = () => {
+	let movies = [];
+	return new Promise((resolve, reject) =>{
+		$.ajax(`${firebaseKey.databaseURL}/movies.json?orderBy="uid"&equalTo="${userUid}"`).then((fbMovies) =>{
+			if(fbMovies != null){
+				Object.keys(fbMovies).forEach((key) =>{
+					fbMovies[key].id = key;
+					movies.push(fbMovies[key]);
+				});
+			}
+
+			resolve(movies);
+		}).catch((err) =>{
+			reject(err);
+		});
+	});
+};
 
 
-module.exports = {setKey, authenticateGoogle};
+
+
+
+
+
+
+
+
+
+module.exports = {setKey, authenticateGoogle, getMovieList};
